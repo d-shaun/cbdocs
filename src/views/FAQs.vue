b3a063e4bef8f629a404d4ec6e739f4649bb6bc2
Shaun <ayy@nope.com>
2020-07-08 04:45:19 -0400

    fx reference links

--- src/views/FAQs.vue
+++ src/views/FAQs.vue
@@ -11,6 +11,22 @@
         <v-expansion-panel-header class="font-weight-bold">{{howto.title}}</v-expansion-panel-header>
         <v-expansion-panel-content>
           <div v-html="howto.text"></div>
+          <div v-if="howto.references">
+            <v-divider class="my-3"></v-divider>
+            <div class="caption">
+              References:
+              <ul class="my-1">
+                <li
+                  v-for="(reference, i) in howto.references"
+                  :key="reference"
+                  style="list-style: none;"
+                >
+                  {{i+1}}.
+                  <router-link :to="reference">{{parse_reference(reference)}}</router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -36,13 +52,15 @@ export default {
           id: "login",
           title: "How to login to/logout of the bot?",
           text:
-            "To login to the bot, you need to <code>&login [username]</code>, <sup><a href='/commands#login'>[ref]</a></sup> replacing <code>[username]</code> with your Last.fm username; example: <code>&login JohnDoe</code>. And, to logout, simply <code>&logout</code>. <sup><a href='/commands#logout'>[ref]</a></sup>"
+            "To login to the bot, you need to <code>&login [username]</code>, <sup>[1]</sup> replacing <code>[username]</code> with your Last.fm username; example: <code>&login JohnDoe</code>. And, to logout, simply <code>&logout</code>. <sup>[2]</sup>",
+          references: ["/commands#login", "/commands#logout"]
         },
         {
           id: "erasecrowns",
           title: "How do I erase my crowns from the bot?",
           text:
-            "You can <code>&deletecrowns</code> to delete your crowns in a server, and <code>&deletecrowns global</code> to delete all of your crowns in every server you have crowns in.<sup><a href='/commands#deletecrowns'>[ref]</a></sup>"
+            "You can <code>&deletecrowns</code> to delete your crowns in a server, and <code>&deletecrowns global</code> to delete all of your crowns in every server you have crowns in.<sup>[1]</sup>",
+          references: ["/commands#deletecrowns"]
         },
         {
           id: "betacommands",
@@ -50,11 +68,17 @@ export default {
           text: `Beta commands are generally unstable commands with some of them having similar aliases to FMCord's commands.
             <br> To access them, you can either: 
             <ul>
-            <li>enable beta commands in the server with <code>&beta</code>; <sup><a href='/commands#beta'>[ref]</a></sup></li>
-            <li>prepend <code>b:</code> before the command name; examples: <code>&b:list</code>, <code>&b:taste</code>.
+            <li>enable beta commands in the server with <code>&beta</code>; <sup>[1]</sup></li>
+            <li>prepend <code>b:</code> before the command name; examples: <code>&b:list</code>,<sup>[2]</sup> <code>&b:taste</code>. <sup>[3]</sup>
             </ul>
-            To see a list of the beta commands: <code>&commands beta</code>.
-            `
+            To see a list of the beta commands: <code>&commands beta</code>. <sup>[4]</sup>
+            `,
+          references: [
+            "/commands#beta",
+            "/commands#list",
+            "/commands#taste",
+            "/commands#commands"
+          ]
         },
         {
           id: "reportbugs",
@@ -78,6 +102,13 @@ export default {
     }
   },
   methods: {
+    parse_reference: function(reference) {
+      const command = reference.split("#");
+      if (command[0] === "/commands") {
+        return "&" + command[1] + " command";
+      }
+      return reference;
+    },
     handleURL: function(id) {
       if (this.$route.hash !== "#" + id) this.$router.push({ hash: id });
     }
